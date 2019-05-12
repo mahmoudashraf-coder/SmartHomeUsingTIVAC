a6548a500b8344588c6c759e4f84a5e57383fe88
Giovanni Mounir <giovannimounir@gmail.com>
2019-05-12 14:51:49 +0200

    Implement MCAL/Timer (see desc)
    1. SysTick_Init: Function to initialize SysTick
    2. delay_MS: Function to use SysTick for delays in milliseconds.
    3. Add tm4c123gh6pm.h: contains TM4C123 registers

---
+++ MCAL/TIMER.C
@@ -0,0 +1,17 @@
+#include "tm4c123gh6pm.h"
+void SysTick_Init(void){
+	NVIC_ST_CTRL_R = 0; // 1) disable SysTick during setup
+	NVIC_ST_RELOAD_R = 0x00FFFFFF; // 2) maximum reload value
+	NVIC_ST_CURRENT_R = 0; // 3) any write to current clears it
+	NVIC_ST_CTRL_R = 0x00000005; // 4) enable SysTick with core clock
+}
+void delay_MS(unsigned long delay){
+	int i;
+	SysTick_Init();	// delay is in 12.5ns units
+	for (i=0;i<delay;i++){
+		NVIC_ST_RELOAD_R = (80*1000)-1; // number of counts to wait
+		NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears
+		while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for count flag
+		}
+	}
+}
---
+++ MCAL/TIMER.h
@@ -0,0 +1,5 @@
+#ifndef __ARMCORTEX_TIMER__
+#define __ARMCORTEX_TIMER__
+void SysTick_Init(void);
+void delay_MS(unsigned long delay);
+#endif
